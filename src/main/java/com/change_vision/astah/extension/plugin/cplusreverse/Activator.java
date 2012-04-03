f344b73b17e2d3f4c6f60dac194a9deba1447c8f
zhouyi <yi.zhou@change-vision.com>
2012-04-03 15:29:40 +0900

    replace source code

--- src/main/java/com/change_vision/astah/extension/plugin/cplusreverse/Activator.java
+++ src/main/java/com/change_vision/astah/extension/plugin/cplusreverse/Activator.java
@@ -3,13 +3,39 @@ package com.change_vision.astah.extension.plugin.cplusreverse;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+
+import com.change_vision.jude.api.inf.ui.IMessageDialogHandler;
+import com.change_vision.jude.api.inf.ui.IMessageDialogHandlerFactory;
 
 public class Activator implements BundleActivator {
-
+	private static IMessageDialogHandler messageHandler;
+	private ServiceReference reference;
+	
 	public void start(BundleContext context) {
+		EditionChcker checker = new EditionChcker();
+		if(checker.hasError()) {
+			return;
+		}
+		ServiceTracker tracker = new ServiceTracker(context, IMessageDialogHandlerFactory.class.getName(), null);
+		tracker.open();
+		initializeMessageDialogHandler(context);
 	}
-
+	
 	public void stop(BundleContext context) {
 	}
 	
+	private void initializeMessageDialogHandler(BundleContext context) {
+		reference = context.getServiceReference(IMessageDialogHandlerFactory.class.getName());
+		IMessageDialogHandlerFactory factory = (IMessageDialogHandlerFactory)context.getService(reference);
+		if(factory != null) {
+			messageHandler = factory.createMessageDialogHandler(new Messages(), "\\.astah\\professional|uml\\cplusreverse.log");
+		}
+		context.ungetService(reference);
+	}
+	
+	public static IMessageDialogHandler getMessageHandler() {
+		return messageHandler;
+	}
 }
